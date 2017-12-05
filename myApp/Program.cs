489fe9b0484c9bfca751d31138b247a0ea768068
Wilson <wilson@enclave.vn>
2017-12-05 16:37:06 +0700

    xoa 2 dong in ra man hinh

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -6,8 +6,6 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello masster!");
         }
     }
